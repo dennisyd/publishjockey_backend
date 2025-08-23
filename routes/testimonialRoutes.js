b1a4b75a01bc9350980e50ca6bdc8b69dc299662
dennisyd <31513813+dennisyd@users.noreply.github.com>
2025-08-23 10:33:06 -0400

    Fix admin testimonial

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -6,22 +6,11 @@ const { verifyToken, requireAdmin } = require('../middleware/auth');
 // Public: submit testimonial
 router.post('/', testimonialController.submitTestimonial);
 
-// Public: get approved testimonials, Admin: get all testimonials
-router.get('/', (req, res, next) => {
-  // If ?approved=true, show only approved (public access)
-  if (req.query.approved === 'true') {
-    return testimonialController.getApprovedTestimonials(req, res, next);
-  }
-  // For getting all testimonials, require admin authentication
-  return verifyToken(req, res, (err) => {
-    if (err) {
-      return res.status(401).json({ error: 'Authentication required' });
-    }
-    return requireAdmin(req, res, () => {
-      return testimonialController.getAllTestimonials(req, res, next);
-    });
-  });
-});
+// Public: get approved testimonials
+router.get('/', testimonialController.getApprovedTestimonials);
+
+// Admin: get all testimonials (including unapproved)
+router.get('/admin', verifyToken, requireAdmin, testimonialController.getAllTestimonials);
 
 // Admin: update testimonial
 router.patch('/:id', verifyToken, requireAdmin, testimonialController.updateTestimonial);
